d6a822634c82b37ce07ba1e196e7bfdd34f12f7c
OgnennuyVladuka <146809006+OgnennuyVladuka@users.noreply.github.com>
2023-10-08 22:31:18 +0300

    Add files via upload

---
+++ z1l/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace z1l
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            int g = 0;
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    g = i * j;
+                    s = String.Format("{0,-10}", i + " * " + j + " = " + g);
+                    Console.Write("  " + s);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
